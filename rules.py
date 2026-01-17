a59b71a203004f06ccd2e2a12c3c2a4c38bcd311
Anamika-cyberspace <144151012+anamika1101@users.noreply.github.com>
2026-01-17 17:39:15 +0530

    Delete rules.py

--- rules.py
+++
@@ -1,6 +0,0 @@
-ATTACK_RULES = {
-    "SQL Injection": ["' OR 1=1", "--", "UNION SELECT"],
-    "XSS": ["<script>", "</script>"],
-    "Command Injection": ["cmd.exe", "powershell", "; ls"],
-    "Directory Traversal": ["../", "..\\"]
-}
