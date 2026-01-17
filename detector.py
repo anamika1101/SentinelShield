e24436e59999ef6162519fd0c3fce587ef0d9bc6
Anamika-cyberspace <144151012+anamika1101@users.noreply.github.com>
2026-01-17 17:38:14 +0530

    Delete detector.py

--- detector.py
+++
@@ -1,8 +0,0 @@
-from rules import ATTACK_RULES
-
-def detect_attack(payload):
-    for attack, patterns in ATTACK_RULES.items():
-        for pattern in patterns:
-            if pattern.lower() in payload.lower():
-                return attack
-    return None
